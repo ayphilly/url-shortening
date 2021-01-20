322e76da7b9643aacd81cf35574da882b3289d15
waleofblanco <mac@macs-MacBook-Pro.local>
2021-01-20 14:41:37 +0100

    included the api call to shortcode url

--- src/components/inputshorten.js
+++ src/components/inputshorten.js
@@ -4,26 +4,29 @@ import "./inputshorten.css"
 function InputandShorten () {
 
     const [url, setUrl] = useState('');
-
+    const [resultUrl, setResult] = useState({})
+    const shortenUrl = `https://api.shrtco.de/v2/shorten?url=${url}`
     const handleUserInput = (e) => {
 
         var url = e.target.value;
         setUrl(url);
 
     }
-    const handleSubmit = (e) => {
-        alert("url to shorten is " + url);
+
+    async function handleSubmit(e) {
         e.preventDefault();
+        var response = await fetch(shortenUrl);
+        var result = await response.json();
+        setResult(result)
     }
+    
     return (
         <div className="input-shorten">
             <form onSubmit={ (e)=> handleSubmit(e)}>
                 <input type="text" name="url" className="url-shorten" placeholder="Shorten a link here" onChange={ (e)=> handleUserInput(e) }/>
                 <button type="submit" className="shorten" > Shorten </button>
             </form>
-            
-            
-
+            <p>{ resultUrl.ok && resultUrl.result.short_link}</p>
         </div>
     )
 }
